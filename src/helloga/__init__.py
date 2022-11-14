6ffbf7649cfcdfd00a85c6864513559533c9e726
rexzhang2014 <rexzhang2013@gmail.com>
2022-11-14 14:54:16 +0800

    add example

---
+++ src/helloga/__init__.py
@@ -0,0 +1,7 @@
+
+from helloga.individual import *
+from helloga.fitness import *
+from helloga.crossover import *
+from helloga.selector import *
+from helloga.environment import *
+import pandas as pd 
